155e7c763a4e286cf6c43b1bfc837b2dfa559763
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-12-24 17:58:20 +0000

    Added ability to specify offset folder for running local local tests.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2652 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- config/make/plugins/ruby.mk
+++ config/make/plugins/ruby.mk
@@ -5,6 +5,11 @@ define ruby.add-item-to-loadpath
   ruby.LOAD_PATH += $(1)
 endef
 
+define util.set-variable
+  ${call core.info,ruby,Setting variable $(1) to $(2)}
+  override $(1) = $(2)
+endef
+
 define ruby.add-cwd-to-loadpath
   ${eval ${call ruby.add-item-to-loadpath,${dir $(core.LAST_LOADED_FILE)}}}
 endef
@@ -13,7 +18,12 @@ define ruby.add-path-to-loadpath
   ${eval ${call ruby.add-item-to-loadpath,$(PATH)}}
 endef
 
+define ruby.set-local-offset
+  ${if ${findstring true,$(OFFSET)},${eval ${call util.set-variable,ruby.LOCAL_OFFSET,$(1)}}}
+endef
+
 ITEMS ?= *.rb
+ruby.LOCAL_OFFSET ?= .
 
 ${call core.show-current-location}
 ${foreach item,$(SK_MAKE_MAKEFILES_TO_TOP),${call core.load,$(item)}}
@@ -22,12 +32,12 @@ define ruby.exec
   ruby -rubygems ${addprefix -I,$(ruby.LOAD_PATH)} $(1) || exit $${?}
 endef
 
-all:: 
+all::
 	@ echo "Available targets: test local-test"
 
-test:: 
+test::
 	@ find . -name makefile -print | while read path; do (cd `dirname $${path}` && $(MAKE) local-$(@)) || exit $${?}; done
 
-local-test:: 
+local-test::
 	@ echo Folder: $(PWD)
-	@ ls -1 | while read item; do case $${item} in ${call util.join,$(ITEMS),|}) ${call ruby.exec,$${item}};; esac; done
+	@ for item in $(ruby.LOCAL_OFFSET)/*; do case $${item} in ${call util.join,${addprefix $(ruby.LOCAL_OFFSET)/,$(ITEMS)},|}) ${call ruby.exec,$${item}};; esac; done
