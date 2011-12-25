5ab8e35803f6239564bd8d4fe0cf7580dd24e6a1
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2011-12-25 00:43:41 +0000

    Moved a function into more appropriate place.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2654 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- config/make/plugins/ruby.mk
+++ config/make/plugins/ruby.mk
@@ -5,11 +5,6 @@ define ruby.add-item-to-loadpath
   ruby.LOAD_PATH += $(1)
 endef
 
-define util.set-variable
-  ${call core.info,ruby,Setting variable $(1) to $(2)}
-  override $(1) = $(2)
-endef
-
 define ruby.add-cwd-to-loadpath
   ${eval ${call ruby.add-item-to-loadpath,${dir $(core.LAST_LOADED_FILE)}}}
 endef
@@ -22,15 +17,19 @@ define ruby.set-local-offset
   ${if ${findstring true,$(OFFSET)},${eval ${call util.set-variable,ruby.LOCAL_OFFSET,$(1)}}}
 endef
 
-ITEMS ?= *.rb
-ruby.LOCAL_OFFSET ?= .
-
 ${call core.show-current-location}
 ${foreach item,$(SK_MAKE_MAKEFILES_TO_TOP),${call core.load,$(item)}}
+
+ITEMS ?= *.rb
+OFFSET ?= false
+
+ruby.LOCAL_OFFSET ?= .
 
 define ruby.exec
   ruby -rubygems ${addprefix -I,$(ruby.LOAD_PATH)} $(1) || exit $${?}
 endef
+
+local.item-list = ${call util.join,${addprefix $(ruby.LOCAL_OFFSET)/,$(ITEMS)},|}
 
 all::
 	@ echo "Available targets: test local-test"
@@ -40,4 +39,4 @@ test::
 
 local-test::
 	@ echo Folder: $(PWD)
-	@ for item in $(ruby.LOCAL_OFFSET)/*; do case $${item} in ${call util.join,${addprefix $(ruby.LOCAL_OFFSET)/,$(ITEMS)},|}) ${call ruby.exec,$${item}};; esac; done
+	@ for item in $(ruby.LOCAL_OFFSET)/*; do case $${item} in ${local.item-list}) ${call ruby.exec,$${item}};; esac; done
