905ead82bd0dbed01332aae009bf037f92dcb4da
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2012-04-29 07:39:39 +0000

    Adjusted the code to recenlty introduced artifact location figuring facility.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2933 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- config/make/plugins/enum.mk
+++ config/make/plugins/enum.mk
@@ -1,20 +1,13 @@
 # vim: ft=make: sw=2:
-
 ${call core.show-current-location}
-${call core.ensure-defined,SK_MAKE_PROJECT_ROOT,SK_MAKE_PATH_FROM_TOP,SK_MAKE_PATH_TO_TOP}
 
-enum.LOCATION := ${basename $(core.LAST_LOADED_FILE)}
-enum.BINDIR := $(SK_MAKE_PATH_TO_TOP)/../bin/$(SK_MAKE_PATH_FROM_TOP)
-enum.GENERATOR := $(enum.LOCATION)/generate-makefile
-
-$(enum.BINDIR):
-	mkdir -p $@
+enum.p.GENERATOR := $(core.PLUGINS_DIR)/enum/generate-makefile
 
 define enum.generate-makefile
-  $(enum.BINDIR)/$(1): . $(enum.BINDIR) $(enum.GENERATOR) $(core.LAST_LOADED_FILE)
-	@ echo "### Rebuilding $(enum.BINDIR)/$(1)"
-	@ $(enum.GENERATOR) -o "$(enum.BINDIR)/$(1)" -t "$(2)" -c "$(3)" -a "$(5)" "$(4)"
+  $(sys.BINDIR)/$(1): . $(sys.BINDIR) $(enum.p.GENERATOR) $(core.LAST_LOADED_FILE)
+	@ echo "### Rebuilding $(sys.BINDIR)/$(1)"
+	@ $(enum.p.GENERATOR) -o "$(sys.BINDIR)/$(1)" -t "$(2)" -c "$(3)" -a "$(5)" "$(4)"
 
-  ${call core.load-if-present,$(enum.BINDIR)/$(1)}
+  ${call core.load-if-present,$(sys.BINDIR)/$(1)}
 endef
 
--- config/make/plugins/tpm.mk
+++ config/make/plugins/tpm.mk
@@ -1,13 +1,10 @@
 # vim: ft=make: sw=2:
 
 ${call core.show-current-location}
-${call core.ensure-defined,SK_MAKE_PKGTOP}
+${call core.ensure-defined,sys.PKGTOP}
 
 PN_TPM_TARGETS = package clean clean-packages build-package show-packages
 .PHONY: $(PN_TPM_TARGETS)
-
-$(SK_MAKE_PKGTOP):
-	mkdir -p $@
 
 define tpm.private.make-package
   info::
@@ -17,14 +14,14 @@ define tpm.private.make-package
 
   clean:: clean-packages
 
-  build-package:: $(SK_MAKE_PKGTOP) $(core.LAST_LOADED_FILE)
-	$(ruby.ENV) distributor --dump-path -v -o $(SK_MAKE_PKGTOP) $(1) $(2)
+  build-package:: $(sys.PKGTOP) $(core.LAST_LOADED_FILE)
+	$(ruby.ENV) distributor --dump-path -v -o $(sys.PKGTOP) $(1) $(2)
 
   clean-packages::
-	rm -f $(SK_MAKE_PKGTOP)/*.tpm
+	rm -f $(sys.PKGTOP)/*.tpm
 
-  show-packages:: $(SK_MAKE_PKGTOP)
-	@for f in $(SK_MAKE_PKGTOP)/*.tpm; do echo $$$${f}; done
+  show-packages:: $(sys.PKGTOP)
+	@for f in $(sys.PKGTOP)/*.tpm; do echo $$$${f}; done
 endef
 
 define tpm.make-package
