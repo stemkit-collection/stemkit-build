8d539ad46849b9b2bd4c40f345ca341491f10e06
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2012-05-23 23:43:49 +0000

    Setting up source and binary tops for distributor, so that it can find its content correctly.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2949 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- config/make/plugins/tpm.mk
+++ config/make/plugins/tpm.mk
@@ -15,7 +15,7 @@ define tpm.private.make-package
   clean:: clean-packages
 
   build-package:: $(sys.PKGTOP) $(core.LAST_LOADED_FILE)
-	$(ruby.ENV) distributor --dump-path -v -o $(sys.PKGTOP) $(1) $(2)
+	$(ruby.ENV) distributor --source $(sys.SRCTOP) --binary $(sys.BINTOP) --dump-path -v -o $(sys.PKGTOP) $(1) $(2)
 
   clean-packages::
 	rm -f $(sys.PKGTOP)/*.tpm
