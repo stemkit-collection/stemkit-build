8856e4fbbd33d809d7f2825d7133e9175b16b6cf
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2012-11-27 08:47:47 +0000

    Moved static dependencies for tpm support out from the dynamic rule.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@2986 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- config/make/plugins/tpm.mk
+++ config/make/plugins/tpm.mk
@@ -3,28 +3,28 @@
 ${call core.show-current-location}
 ${call core.ensure-defined,sys.PKGTOP}
 
-PN_TPM_TARGETS = package clean clean-packages build-package show-packages
-.PHONY: $(PN_TPM_TARGETS)
+tpm.TARGETS := package clean clean-packages build-package show-packages
+.PHONY: $(tpm.TARGETS)
 
-define tpm.private.make-package
-  info::
-	@echo Available targets: $(PN_TPM_TARGETS)
-
-  package:: build-package
-
-  clean:: clean-packages
-
+define tpm.p.make-package
   build-package:: $(sys.PKGTOP) $(core.LAST_LOADED_FILE)
 	$(ruby.ENV) distributor --source $(sys.SRCTOP) --binary $(sys.BINTOP) --dump-path -v -o $(sys.PKGTOP) $(1) $(2)
-
-  clean-packages::
-	rm -f $(sys.PKGTOP)/*.tpm
-
-  show-packages:: $(sys.PKGTOP)
-	@for f in $(sys.PKGTOP)/*.tpm; do echo $$$${f}; done
 endef
 
 define tpm.make-package
-  ${eval ${call if-local,tpm.private.make-package,$(1),$(2)}}
+  ${eval ${call if-local,tpm.p.make-package,$(1),$(2)}}
 endef
 
+info::
+	@echo Available targets: $(tpm.TARGETS)
+
+$(tmp.TARGETS)::
+
+package:: build-package
+clean:: clean-packages
+
+clean-packages::
+	rm -f $(sys.PKGTOP)/*.tpm
+
+show-packages:: $(sys.PKGTOP)
+	@for f in $(sys.PKGTOP)/*.tpm; do echo $${f}; done
