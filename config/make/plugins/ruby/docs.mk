11649524ed4095971bd760e874589aa7b48768df
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2012-12-20 17:50:44 +0000

    Implemented ability to build only one set of docs for the same name if defined several times in the hierarchy.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@3010 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- config/make/plugins/ruby/docs.mk
+++ config/make/plugins/ruby/docs.mk
@@ -2,24 +2,36 @@
 
 ruby.docs.YARDOPTS := $(core.LAST_LOADED_FROM)/docs/yard-defaults
 ruby.docs.TARGETS := docs redocs install-docs clean-docs
+
 .PHONY: $(ruby.docs.TARGETS)
 
-ruby.docs.include = ${eval ruby.docs.INCLUDE_$(1) += ${call sys.makefile-src,$(2),$(3)}}
+# Public rules
+#
+ruby.docs.setup-api-version = ${eval ${call ruby.docs.p.v,API_VERSION,$(1)} := $(2)}
 
-ruby.docs.front-page = ${eval ruby.docs.FRONT_PAGE_$(1) := ${call sys.makefile-src,$(2),$(3)}}
+ruby.docs.include = ${eval ${call ruby.docs.p.v,INCLUDE,$(1)} += ${call sys.makefile-src,$(2),$(3)}}
 
-ruby.docs.pages = ${eval ruby.docs.PAGES_$(1) += ${call sys.makefile-src,$(2),$(3)}}
+ruby.docs.front-page = ${eval ${call ruby.docs.p.v,FRONT_PAGE,$(1)} := ${call sys.makefile-src,$(2),$(3)}}
 
-ruby.docs.make = ${eval ${call ruby.docs.p.make,$(1),$(sys.PKGTOP)/docs/$(1),$(2),$(3),$(4)}}
+ruby.docs.pages = ${eval ${call ruby.docs.p.v,PAGES,$(1)} += ${call sys.makefile-src,$(2),$(3)}}
+
+ruby.docs.make = ${if $(ruby.docs.MAKE_$(1)),,${eval ${call ruby.docs.p.make,$(1),${call ruby.docs.p.target,$(1)},$(2),$(3),$(4)}}}
+
+# Private rules
+#
+ruby.docs.p.v = ruby.docs.$(1)_$(2)_$(core.LAST_LOADED_FROM)
+
+ruby.docs.p.target = $(sys.PKGTOP)/docs/$(1)$(${call ruby.docs.p.v,API_VERSION,$(1)}:%=-%)
 
 define ruby.docs.p.make
+  ruby.docs.MAKE_$(1) := true
   ${call ruby.docs.include,$(1),$(3),$(4)}
 
   $(2):
 	mkdir -p $$(@)
 
   docs:: $(2)
-	$(ruby.ENV) yard doc -o $$(<) -b $$(<)/.yardoc --yardopts $(ruby.docs.YARDOPTS) $$(ruby.docs.FRONT_PAGE_$(1):%=--main '%') $$(ruby.docs.INCLUDE_$(1):%='%') - $$(ruby.docs.PAGES_$(1):%='%')
+	$(ruby.ENV) yard doc -o $$(<) -b $$(<)/.yardoc --yardopts $(ruby.docs.YARDOPTS) $$(${call ruby.docs.p.v,FRONT_PAGE,$(1)}:%=--main '%') $$(${call ruby.docs.p.v,INCLUDE,$(1)}:%='%') - $$(${call ruby.docs.p.v,PAGES,$(1)}:%='%')
 
   install-docs:: $(2)
 	mkdir -p $${dir $$(LOCATION)/$(1)}
