d6497d581ba2d7ca6d72ce29ca75d70cde83af12
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2012-12-20 09:17:27 +0000

    Simplified docs generation and made it possible to reference files from where the makefile containing docs generation rules is located.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@3009 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- config/make/plugins/ruby/docs.mk
+++ config/make/plugins/ruby/docs.mk
@@ -1,48 +1,32 @@
 # vim: ft=make: sw=2:
 
-ruby.docs.YARDOPTS := ${dir $(core.LAST_LOADED_FILE)}docs/yard-defaults
+ruby.docs.YARDOPTS := $(core.LAST_LOADED_FROM)/docs/yard-defaults
 ruby.docs.TARGETS := docs redocs install-docs clean-docs
 .PHONY: $(ruby.docs.TARGETS)
 
-define ruby.docs.include
-  ${eval ruby.docs.INCLUDE_$(1) += ${call util.join-path,$(2),$(3)}}
-endef
+ruby.docs.include = ${eval ruby.docs.INCLUDE_$(1) += ${call sys.makefile-src,$(2),$(3)}}
 
-define ruby.docs.front-page
-  ${eval ruby.docs.FRONT_PAGE_$(1) := ${call util.join-path,$(2),$(3)}}
-endef
+ruby.docs.front-page = ${eval ruby.docs.FRONT_PAGE_$(1) := ${call sys.makefile-src,$(2),$(3)}}
 
-define ruby.docs.pages
-  ${eval ruby.docs.PAGES_$(1) += ${call util.join-path,$(2),$(3)}}
-endef
+ruby.docs.pages = ${eval ruby.docs.PAGES_$(1) += ${call sys.makefile-src,$(2),$(3)}}
 
-define ruby.docs.p.join
-  $$(ruby.docs.$(2)_$(3):%=$(1:%=% )'$(sys.SRCTOP)/%')
-endef
-
-define ruby.docs.p.top
-  $(sys.PKGTOP)/docs$(1:%=/%)$(2:%=/%)
-endef
+ruby.docs.make = ${eval ${call ruby.docs.p.make,$(1),$(sys.PKGTOP)/docs/$(1),$(2),$(3),$(4)}}
 
 define ruby.docs.p.make
-  ${call ruby.docs.include,$(1),$(2),$(3)}
+  ${call ruby.docs.include,$(1),$(3),$(4)}
 
-  ${call ruby.docs.p.top,$(1)}:
+  $(2):
 	mkdir -p $$(@)
 
-  docs:: ${call ruby.docs.p.top,$(1)}
-	$(ruby.ENV) yard doc -o $$(<) -b $$(<)/.yardoc --yardopts $(ruby.docs.YARDOPTS) ${call ruby.docs.p.join,--main,FRONT_PAGE,$(1)} ${call ruby.docs.p.join,,INCLUDE,$(1)} - ${call ruby.docs.p.join,,PAGES,$(1)}
+  docs:: $(2)
+	$(ruby.ENV) yard doc -o $$(<) -b $$(<)/.yardoc --yardopts $(ruby.docs.YARDOPTS) $$(ruby.docs.FRONT_PAGE_$(1):%=--main '%') $$(ruby.docs.INCLUDE_$(1):%='%') - $$(ruby.docs.PAGES_$(1):%='%')
 
-  install-docs:: ${call ruby.docs.p.top,$(1)}
-	rm -rf $$(LOCATION)/$${notdir $$(<)}
-	cp -r $$(<) $$(LOCATION)/.
+  install-docs:: $(2)
+	mkdir -p $${dir $$(LOCATION)/$(1)}
+	cp -r $$(<) $${dir $$(LOCATION)/$(1)}
 
   clean-docs::
-	rm -rf ${call ruby.docs.p.top,$(1)}
-endef
-
-define ruby.docs.make
-  ${eval ${call ruby.docs.p.make,$(1),$(2),$(3),$(4)}}
+	rm -rf $(2)
 endef
 
 info::
