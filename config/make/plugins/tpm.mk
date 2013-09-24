9cd9aa8e81db999f1cc1b029032f1bcce8942f0e
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2013-09-24 20:09:33 +0000

    Printing available tpm creation targets for "make info".
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@3065 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- config/make/plugins/tpm.mk
+++ config/make/plugins/tpm.mk
@@ -17,6 +17,9 @@ endef
 
 define tpm.p.make
   .PHONY: $(2)-tpm
+  info::
+	@ echo Available targets: $(2)-tpm
+
   $(2)-tpm:: $(sys.PKGTOP) $(core.LAST_LOADED_FILE)
 	ruby-env 1.8 distributor --dump-path -v $(3:%=-V%) $(4:%=-B%) --source $(sys.SRCTOP) --binary $(sys.BINTOP) -o $(sys.PKGTOP) prodinfo-$(1) ${if $(5),$(5),$(2)}
 endef
@@ -25,6 +28,9 @@ define tpm.p.make-rpm
   ${call tpm.p.make,$(1),$(2),$(4),$(5),$(6)}
 
   .PHONY: $(2)-rpm $(2)-rpm-only
+  info::
+	@ echo Available targets: $(2)-rpm $(2)-rpm-only
+
   $(2)-rpm:: $(2)-tpm $(2)-rpm-only
   $(2)-rpm-only::
 	tpm-to-rpm --dump-path $(4:%=-V%) $(5:%=-B%) $(7:%=-t %) -N $(1)$(3:%=-%) -s $(1).rpm-spec -f $(sys.PKGTOP)/${if $(6),$(6),$(2)}.tpm-path -r $(1)-responses.yaml
