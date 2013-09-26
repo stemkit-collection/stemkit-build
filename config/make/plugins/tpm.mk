60f4a8a13d2eedef213935d60d7fddb5e719c628
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2013-09-26 16:52:36 +0000

    Drastically simplified make rules for TPM and RPM builds.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@3071 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- config/make/plugins/tpm.mk
+++ config/make/plugins/tpm.mk
@@ -21,11 +21,11 @@ define tpm.p.make
 	@ echo Available targets: $(2)-tpm
 
   $(2)-tpm:: $(sys.PKGTOP) $(core.LAST_LOADED_FILE)
-	ruby-env 1.8 distributor -v --dump-path $(3:%=-V%) $(4:%=-B%) --source $(sys.SRCTOP) --binary $(sys.BINTOP) -o $(sys.PKGTOP) prodinfo-$(1) ${if $(5),$(5),$(2)}
+	ruby-env 1.8 distributor -v --dump-path $(3:%=-V%) $(4:%=-B%) --source $(sys.SRCTOP) --binary $(sys.BINTOP) -o $(sys.PKGTOP) prodinfo-$(1) $(2)
 endef
 
 define tpm.p.make-rpm
-  ${call tpm.p.make,$(1),$(2),$(4),$(5),$(6)}
+  ${call tpm.p.make,$(1),$(2),$(3),$(4)}
 
   .PHONY: $(2)-rpm $(2)-rpm-only
   info::
@@ -33,29 +33,16 @@ define tpm.p.make-rpm
 
   $(2)-rpm:: $(2)-tpm $(2)-rpm-only
   $(2)-rpm-only::
-	tpm-to-rpm -v --dump-path $(4:%=-V%) $(5:%=-B%) $(7:%=-t%) -N $(1)$(3:%=-%) -c rpm/$(1).conf -f $(sys.PKGTOP)/${if $(6),$(6),$(2)}.tpm-path
+	tpm-to-rpm -v --dump-path $(3:%=-V%) $(4:%=-B%) $(6:%=-t%) -N $(5:%=%-)$(2) -c rpm/$(1).conf -f $(sys.PKGTOP)/$(2).tpm-path
 endef
 
-# ${call tpm.make, <product> [<package>], <version>, <build>, [<package>]}
-#                  $(1)                   $(2)       $(3)     $(4)
-#
-# $(1)[last] is used for constructing a target, hence when <product> is to be
-# used for that, omit <package> in $(1), specifying it in $(4). Othewise, if
-# the target must be based on <package>, specify both <product> and <package>
-# in $(1), omitting $(4) altogether.
-#
-tpm.make = ${eval ${call if-local,tpm.p.make,${firstword $(1)},${lastword $(1)},$(2),$(3),$(4:%=%)}}
+# ${call tpm.make, <product>, <package>, <version>, <build>
+#                  $(1)       $(2)       $(3)       $(4)
+tpm.make = ${eval ${call if-local,tpm.p.make,$(1:%=%),$(2:%=%),$(3),$(4)}}
 
-# ${call tpm.make-rpm, <product> [<package>], <version>, <build>, [<package>], <top>}
-#                      $(1)                   $(2)       $(3)     $(4)         $(5)
-#
-# Same consideration for specifying <package> either in $(1) or in $(4). When
-# $(4) must be empty and <top> specified, make sure to explicitly use empty
-# value (,,). Additional consideration is that when both <product> and
-# <package> is specified in $(1), the RPM product name (-N) will be set to
-# <product>-<package>. Otherwise, to just <package>.
-#
-tpm.make-rpm = ${eval ${call if-local,tpm.p.make-rpm,${firstword $(1)},${lastword $(1)},${word 2,$(1)},$(2),$(3),$(4:%=%),$(5:%=%)}}
+# ${call tpm.make-rpm, <product>, <package>, <version>, <build>, <prefix>, <top>}
+#                      $(1)       $(2)       $(3)       $(4)     $(5)      $(6)
+tpm.make-rpm = ${eval ${call if-local,tpm.p.make-rpm,$(1:%=%),$(2:%=%),$(3),$(4),$(5:%=%),$(6)}}
 
 info::
 	@echo Available targets: $(tpm.TARGETS)
