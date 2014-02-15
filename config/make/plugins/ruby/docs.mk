7586d04669003358bcbf77ae8f0f4b3dd9977b40
bystr <bystr@faf13d3b-af0d-0410-97ba-fcc57a00092e>
2014-02-15 07:01:22 +0000

    Refactored docs installation rules to introduce ability to do so via a dedicated makefile.
    git-svn-id: https://svn.code.sf.net/p/stemkit/code@3091 faf13d3b-af0d-0410-97ba-fcc57a00092e

--- config/make/plugins/ruby/docs.mk
+++ config/make/plugins/ruby/docs.mk
@@ -18,45 +18,65 @@ ruby.docs.pages = ${eval ${call ruby.docs.p.v,PAGES,$(1)} += ${call sys.makefile
 
 ruby.docs.files = ${eval ${call ruby.docs.p.v,FILES,$(1)} += ${call sys.makefile-src,$(2),$(3),///}}
 
+ruby.docs.deploy = ${eval ${call ruby.docs.p.v,DEPLOY,$(1)} = ${call sys.makefile-src,$(2)}}
+
 ruby.docs.make = ${if $(ruby.docs.MAKE_$(1)),,${eval ${call ruby.docs.p.make,$(1),$(2),$(3)}}}
 
 # Private rules
 #
 ruby.docs.p.v = ruby.docs.$(1)_$(2)_$(core.LAST_LOADED_FROM)
 
-ruby.docs.p.target = $(sys.PKGTOP)/docs/$(1)$(${call ruby.docs.p.v,API_VERSION,$(1)}:%=-%)
-
-ruby.docs.p.install-target = $$(LOCATION)/$(1)$(${call ruby.docs.p.v,API_VERSION,$(1)}:%=/api-%)
+ruby.docs.p.base = $(1)$(${call ruby.docs.p.v,API_VERSION,$(1)}:%=/%)
 
 define ruby.docs.p.make
-  ${call ruby.docs.p.make-targets,$(1),${call ruby.docs.p.target,$(1)},${call ruby.docs.p.install-target,$(1)},$(2),$(3)}
+  ${call ruby.docs.p.make-targets,$(1),$(sys.PKGTOP)/docs,${call ruby.docs.p.base,$(1)},${call ruby.docs.p.v,DEPLOY,$(1)},$(2),$(3)}
 endef
 
-define ruby.docs.p.install-files
+define ruby.docs.p.copy-files
   sh -c 'tar -C $${0} -cf - $${@} | tar -C $(2) -xf -' ${subst ///, ,$(1)}
 
 endef
 
+define ruby.docs.p.install-locally
+  rm -rf $(2)
+  mkdir $(2)
+  cp -r $(1)/. $(2)/.
+
+endef
+
+define ruby.docs.p.clean-locally
+  rm -rf $(1)
+
+endef
+
+define ruby.docs.p.deploy-via-make
+  + $(MAKE) -C $(4) deploy-$(1) SOURCE=${abspath $(2)/$(3)} OFFSET=$(3)
+endef
+
+define ruby.docs.p.figure-deploy
+  ${if $(4),${call ruby.docs.p.deploy-via-make,$(1),$(2),$(3),$(4)},${call ruby.docs.p.$(1)-locally,$(2)/$(3),${call core.ensure,LOCATION}/$(3)}}
+endef
+
 define ruby.docs.p.make-targets
   ruby.docs.MAKE_$(1) := true
-  ${call ruby.docs.include,$(1),$(4),$(5)}
+  ${call ruby.docs.include,$(1),$(5),$(6)}
 
-  $(2):
+  $(2)/$(3):
 	mkdir -p $$(@)
 
-  local-docs:: $(2)
-	$(ruby.ENV) yard doc -o $(2) -b $(2)/.yardoc --yardopts $(ruby.docs.YARDOPTS) $$(${call ruby.docs.p.v,FRONT_PAGE,$(1)}:%=--main '%') $$(${call ruby.docs.p.v,INCLUDE,$(1)}:%='%') - $$(${call ruby.docs.p.v,PAGES,$(1)}:%='%')
-	$$(foreach dir,$$(${call ruby.docs.p.v,FILES,$(1)}),$$(call ruby.docs.p.install-files,$$(dir),$(2)/.))
-
-  local-install-docs:: $(2)
-	mkdir -p $(3)
-	cp -r $(2)/. $(3)/.
+  local-docs:: $(2)/$(3)
+	$(ruby.ENV) yard doc -o $$(<) -b $$(<)/.yardoc --yardopts $(ruby.docs.YARDOPTS) $$(${call ruby.docs.p.v,FRONT_PAGE,$(1)}:%=--main '%') $$(${call ruby.docs.p.v,INCLUDE,$(1)}:%='%') - $$(${call ruby.docs.p.v,PAGES,$(1)}:%='%')
+	$$(foreach dir,$$(${call ruby.docs.p.v,FILES,$(1)}),$$(call ruby.docs.p.copy-files,$$(dir),$$(<)/.))
 
   local-clean-docs::
-	rm -rf $(2)
+	rm -rf $(2)/$(3)
+
+  local-install-docs:: $(2)/$(3)
+	$${call ruby.docs.p.figure-deploy,install,$(2),$(3),$$($(4))}
 
   local-clean-installed-docs::
-	rm -rf $(3)
+	$${call ruby.docs.p.figure-deploy,clean,$(2),$(3),$$($(4))}
+
 endef
 
 info::
@@ -64,7 +84,7 @@ info::
 	@echo Available targets: $(ruby.docs.LOCAL_TARGETS)
 
 local-redocs:: local-clean-docs local-docs
-local-install-docs local-clean-installed-docs:: sys-ensure-LOCATION
+local-install-docs local-clean-installed-docs::
 
 # In the following actions we want to avoid processing 'docs' in the current
 # folder in find command. This folder is processed by the preceeding make
